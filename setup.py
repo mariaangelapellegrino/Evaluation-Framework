5e62935c6b60f55982c52a9c86874aa83ba75a6a
Maria Angela Pellegrino <mariaangelapellegrino94@gmail.com>
2019-12-24 16:42:14 +0100

    Fixed long description content type

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 
 from distutils.core import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
   name = 'evaluation_framework',         # How you named your package folder (MyLib)
   packages = ['evaluation_framework'],   # Chose the same as "name"
   version = '01.7',      # Start with a small number and increase it with every change you make
   license='apache-2.0',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Evaluation Framework for testing and comparing graph embedding techniques',   # Give a short description about your library
-  long_description=open("./README.md").read(),
+  long_description=long_description,
   long_description_content_type="text/markdown",
   author = 'Maria Angela Pellegrino',                   # Type in your name
   author_email = 'mariaangelapellegrino94@gmail.com',      # Type in your E-Mail
