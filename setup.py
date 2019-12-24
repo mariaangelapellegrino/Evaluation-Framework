fa7fe8f9de30eb2b435fe0c3963e53cedc5d2103
Maria Angela Pellegrino <mariaangelapellegrino94@gmail.com>
2019-12-24 16:38:26 +0100

    Fixed description content format in setup file

--- setup.py
+++ setup.py
@@ -7,17 +7,18 @@ setup(
   license='apache-2.0',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Evaluation Framework for testing and comparing graph embedding techniques',   # Give a short description about your library
   long_description=open("./README.md").read(),
+  long_description_content_type="text/markdown",
   author = 'Maria Angela Pellegrino',                   # Type in your name
   author_email = 'mariaangelapellegrino94@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/mariaangelapellegrino/Evaluation-Framework',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/mariaangelapellegrino/Evaluation-Framework/archive/v_01.7.tar.gz',    # I explain this later on
   keywords = ['evaluation', 'graph-embedding', 'python', 'library', 'benchmark-framework', 'machine learning tasks', 'semantic tasks'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'numpy',
-          'pandas',
-          'scikit-learn',
-          'scipy',
-          'h5py',
+          'numpy==1.14.0',
+          'pandas==0.22.0',
+          'scikit-learn==0.19.2',
+          'scipy==1.1.0',
+          'h5py==2.8.0',
           'unicodecsv'
       ],
   classifiers=[
