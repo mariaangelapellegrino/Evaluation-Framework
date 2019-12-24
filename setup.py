a364109f57021c78665a01eed429a330f18f8db5
Maria Angela Pellegrino <mariaangelapellegrino94@gmail.com>
2019-12-24 12:19:02 +0100

    Update setup file

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
   name = 'evaluation_framework',         # How you named your package folder (MyLib)
   packages = ['evaluation_framework'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '01.1',      # Start with a small number and increase it with every change you make
   license='apache-2.0',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Evaluation Framework for testing and comparing graph embedding techniques',   # Give a short description about your library
   author = 'Maria Angela Pellegrino',                   # Type in your name
   author_email = 'mariaangelapellegrino94@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/mariaangelapellegrino/Evaluation-Framework',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/mariaangelapellegrino/Evaluation-Framework/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/mariaangelapellegrino/Evaluation-Framework/archive/v_01.1.tar.gz',    # I explain this later on
   keywords = ['evaluation', 'graph-embedding', 'python', 'library', 'benchmark-framework', 'machine learning tasks', 'semantic tasks'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'numpy',
@@ -22,7 +22,7 @@ setup(
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: Apache Software License v2.0',   # Again, pick a license
+    'License :: OSI Approved :: Apache License, Version 2.0 (Apache-2.0)',   # Again, pick a license
     'Programming Language :: Python :: 2.7',      #Specify which pyhton versions that you want to support
   ],
 )
