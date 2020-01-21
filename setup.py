f4d6b25a7a433073c5756468043c732937619f7d
marpel <mariaangelapellegrino94@gmail.com>
2020-01-21 17:31:54 +0100

    Fix data upload

--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
 from setuptools import setup, find_packages
 
-with open('README.md', 'r') as f:
+with open('pip_readme.md', 'r') as f:
     long_description = f.read()
 
 setup(
   name = 'evaluation_framework',         
   packages=find_packages(),   
-  packages_data={'evaluation_framework' : ['*/data/*']},
-  version = '1.1',      
+  include_package_data=True,
+  #packages_data={"": ["data/*.tsv", "data/*.csv", "data/*.txt"]},
+  version = '1.3',      
   license='apache-2.0',        
   description = 'Evaluation Framework for testing and comparing graph embedding techniques',  
   long_description=long_description,
   author = 'Maria Angela Pellegrino',                   
   author_email = 'mariaangelapellegrino94@gmail.com',      
   url = 'https://github.com/mariaangelapellegrino/Evaluation-Framework',   
-  download_url = 'https://github.com/mariaangelapellegrino/Evaluation-Framework/archive/v_1.1.tar.gz',    
+  download_url = 'https://github.com/mariaangelapellegrino/Evaluation-Framework/archive/v_1.3.tar.gz',    
   keywords = ['evaluation', 'graph-embedding', 'python', 'library', 'benchmark-framework', 'machine learning tasks', 'semantic tasks'],   # Keywords that define your package best
   install_requires=[           
           'numpy==1.14.0',
