aa4a63e818e58f586adedbbc19f88aec8db00480
janothan <jan.portisch@gmail.com>
2021-05-04 11:03:40 +0200

    Typing of evaluation function

--- evaluation_framework/manager.py
+++ evaluation_framework/manager.py
@@ -3,14 +3,18 @@ import xml.etree.ElementTree as ET
 from evaluation_framework.evaluationManager import EvaluationManager
 from evaluation_framework.txt_dataManager import DataManager as TxtDataManager
 from evaluation_framework.hdf5_dataManager import DataManager as Hdf5DataManager
+from typing import List, Callable
+import numpy as np
 
-available_tasks = ['Classification', 'Regression', 'Clustering', 
-    'DocumentSimilarity', 'EntityRelatedness', 'SemanticAnalogies']
+available_tasks = ['Classification', 'Regression', 'Clustering',
+                   'DocumentSimilarity', 'EntityRelatedness', 'SemanticAnalogies']
 available_file_formats = ['txt', 'hdf5']
 
 """
 It checks the parameters of the evaluation and starts it.
 """
+
+
 class FrameworkManager():
     def __init__(self):
         print('Start evaluation...')
@@ -29,10 +33,11 @@ class FrameworkManager():
     debugging_mode: {True, False}, True to run the tasks by reporting all the information collected during the run, False otherwise. Default: False
     analogy_function: function to compute the analogy among vectors. Default: None to use the default function.
     """
-    def evaluate(self, vector_filename, vector_file_format='txt', vector_size=200, 
-        parallel=False, tasks=available_tasks, similarity_metric='cosine', top_k=2, 
-        compare_with='_all', debugging_mode=False, analogy_function=None ):
 
+    def evaluate(self, vector_filename: str, vector_file_format: str = 'txt', vector_size: int = 200,
+                 parallel: bool = False, tasks: List[str] = available_tasks, similarity_metric: str = 'cosine',
+                 top_k: int = 2, compare_with: str = '_all', debugging_mode: bool = False,
+                 analogy_function: Callable[[np.ndarray, np.ndarray, np.ndarray], np.ndarray] = None):
         self.vector_filename = vector_filename
         self.vector_file_format = vector_file_format
         self.vector_size = vector_size
@@ -50,7 +55,7 @@ class FrameworkManager():
             self.dataManager = TxtDataManager(self.debugging_mode)
         elif vector_file_format == 'hdf5':
             self.dataManager = Hdf5DataManager(self.debugging_mode)
-                    
+
         self.evaluation_manager = EvaluationManager(self.dataManager, self.debugging_mode)
 
         self.evaluation_manager.create_result_directory()
@@ -58,42 +63,46 @@ class FrameworkManager():
         self.evaluation_manager.initialize_vectors(vector_filename, vector_size)
 
         if parallel:
-            scores_dictionary = self.evaluation_manager.run_tests_in_parallel(tasks, similarity_metric, top_k, analogy_function)
+            scores_dictionary = self.evaluation_manager.run_tests_in_parallel(tasks, similarity_metric, top_k,
+                                                                              analogy_function)
         else:
-            scores_dictionary = self.evaluation_manager.run_tests_in_sequential(tasks, similarity_metric, top_k, analogy_function)        
-        
+            scores_dictionary = self.evaluation_manager.run_tests_in_sequential(tasks, similarity_metric, top_k,
+                                                                                analogy_function)
+
         self.evaluation_manager.compare_with(compare_with, scores_dictionary)
-    
+
     """
     It checks if the parameters are all valid. 
     If no problem occurs, the evaluation will start.
     """
+
     def check_parameters(self):
-        if self.vector_filename==None:
+        if self.vector_filename is None:
             raise Exception('The vector filename is a mandatory parameter.')
 
         if not self.vector_file_format in available_file_formats:
             raise Exception('Not supported file format. The managed file format are: ' + available_file_formats)
-        
+
         if self.vector_size < 0:
             raise Exception('The vector size must be not negative.')
-        
-        if self.parallel!=True and self.parallel!=False:
+
+        if self.parallel != True and self.parallel != False:
             raise Exception('The parameter PARALLEL is boolean.')
-        
-        if self.tasks!='_all':
+
+        if self.tasks != '_all':
             for task in self.tasks:
                 if not task in available_tasks:
-                    raise Exception(task + ' is not a supported task. The managed tasks are ' + ', '.join(available_tasks) + ' or \'_all\'.')
-        
-        #similarity_metric TODO
+                    raise Exception(task + ' is not a supported task. The managed tasks are ' + ', '.join(
+                        available_tasks) + ' or \'_all\'.')
+
+        # similarity_metric TODO
         if self.top_k < 0:
             raise Exception('The top_k value must be not negative.')
-        
-        #compare_with TODO
 
-        if self.debugging_mode!=True and self.debugging_mode!=False:
-            raise Exception('The parameter DEBUGGING_MODE is boolean.') 
+        # compare_with TODO
+
+        if self.debugging_mode != True and self.debugging_mode != False:
+            raise Exception('The parameter DEBUGGING_MODE is boolean.')
 
     """
     It reads a xml_file and it recovers the parameters which can be passed in input to the evaluate() function.
@@ -101,9 +110,10 @@ class FrameworkManager():
     
     xml_file: path of the xml file.
     """
+
     def get_parameters_xmlFile(self, xml_file):
         parameters_dict = {}
-        
+
         tree = ET.parse(xml_file)
         root = tree.getroot()
 
@@ -113,14 +123,14 @@ class FrameworkManager():
             actual_tag = root.find(tag)
             if not actual_tag is None:
                 parameters_dict[tag] = actual_tag.text
-                
+
         int_tags = ['vector_size', 'top_k']
 
         for tag in int_tags:
             actual_tag = root.find(tag)
             if not actual_tag is None:
                 parameters_dict[tag] = int(actual_tag.text)
-                
+
         boolean_tags = ['parallel', 'debugging_mode']
 
         for tag in boolean_tags:
@@ -136,7 +146,5 @@ class FrameworkManager():
                 for actual_tag in actual_tag_list.findall('value'):
                     tag_values_list.append(actual_tag.text)
                 parameters_dict[tag] = tag_values_list
-                
-        
 
         return parameters_dict
